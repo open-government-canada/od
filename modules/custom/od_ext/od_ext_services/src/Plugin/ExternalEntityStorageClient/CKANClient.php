8224f4a5531dee0d3f198c17b9407f79f5d963fb
William Hearn <sylus1984@gmail.com>
2017-04-24 19:54:30 -0400

    feat(services): Addition of jsonapi + defaults for external_entities

---
+++ modules/custom/od_ext/od_ext_services/src/Plugin/ExternalEntityStorageClient/CKANClient.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Drupal\od_ext_services\Plugin\ExternalEntityStorageClient;
+
+use Drupal\external_entities\ExternalEntityInterface;
+use Drupal\external_entities\ExternalEntityStorageClientBase;
+
+/**
+ * CKAN implementation of an external entity storage client.
+ *
+ * @ExternalEntityStorageClient(
+ *   id = "ckan_client",
+ *   name = "CKAN"
+ * )
+ */
+class CKANClient extends ExternalEntityStorageClientBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function delete(ExternalEntityInterface $entity) {
+    $this->httpClient->delete(
+      $this->configuration['endpoint'] . '/' . $entity->externalId(),
+      ['headers' => $this->getHttpHeaders()]
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function load($id) {
+    $options = [
+      'headers' => $this->getHttpHeaders(),
+      'query' => [
+        'fq' => 'id:' . $id,
+      ],
+    ];
+    if ($this->configuration['parameters']['single']) {
+      $options['query'] += $this->configuration['parameters']['single'];
+    }
+    $response = $this->httpClient->get(
+      $this->configuration['endpoint'],
+      $options
+    );
+    $result = $this->decoder->getDecoder($this->configuration['format'])->decode($response->getBody());
+    return (object) $result['result']['results'][0];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function save(ExternalEntityInterface $entity) {
+    if ($entity->externalId()) {
+      $response = $this->httpClient->put(
+        $this->configuration['endpoint'] . '/' . $entity->externalId(),
+        [
+          'body' => (array) $entity->getMappedObject(),
+          'headers' => $this->getHttpHeaders(),
+        ]
+      );
+      $result = SAVED_UPDATED;
+    }
+    else {
+      $response = $this->httpClient->post(
+        $this->configuration['endpoint'],
+        [
+          'body' => (array) $entity->getMappedObject(),
+          'headers' => $this->getHttpHeaders(),
+        ]
+      );
+      $result = SAVED_NEW;
+    }
+
+    // @todo: is it standard REST to return the new entity?
+    $object = (object) $this->decoder->getDecoder($this->configuration['format'])->decode($response->getBody());
+    $entity->mapObject($object);
+    return $result;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query(array $parameters) {
+    $response = $this->httpClient->get(
+      $this->configuration['endpoint'],
+      [
+        'query' => $parameters + $this->configuration['parameters']['list'],
+        'headers' => $this->getHttpHeaders(),
+      ]
+    );
+    $results = $this->decoder->getDecoder($this->configuration['format'])->decode($response->getBody());
+    $results = $results['result']['results'];
+    foreach ($results as &$result) {
+      $result = ((object) $result);
+    }
+    return $results;
+  }
+
+}
