2dc521411a0488615d461a46a6426c5a4c21281f
William H <sylus1984@gmail.com>
2017-05-23 22:32:54 -0400

    feat(icon): Add initial integration with icon (#44)

---
+++ modules/custom/od_ext/od_ext_fonts/src/Plugin/Icon/OpenGovProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\od_ext_fonts\Plugin\Icon;
+
+use Drupal\icon\Plugin\Icon\IconProviderBase;
+
+/**
+ * OpenGov.
+ *
+ * @IconProvider(
+ *   id = "opengov",
+ *   label = @Translation("OpenGov"),
+ *   url = "http://open-data.github.io",
+ *   settings = {},
+ * )
+ */
+class OpenGovProvider extends IconProviderBase {
+
+}
---
+++ modules/custom/od_ext/od_ext_fonts/src/Plugin/Icon/OpenGovSet.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\od_ext_fonts\Plugin\Icon;
+
+use Drupal\icon\Plugin\Icon\IconSetBase;
+
+/**
+ * OpenGov Icon Set.
+ *
+ * @IconSet(
+ *   id = "opengov",
+ *   label = @Translation("OpenGov: Icons"),
+ *   icons = {
+ *     "agriculture" = "agriculture",
+ *     "arts" = "arts",
+ *     "citizen" = "citizen",
+ *     "economics" = "economics",
+ *     "education" = "education",
+ *     "government" = "government",
+ *     "health" = "health",
+ *     "history" = "history",
+ *     "info" = "info",
+ *     "labour" = "labour",
+ *     "law" = "law",
+ *     "maps" = "maps",
+ *     "military" = "military",
+ *     "nature" = "nature",
+ *     "projects" = "projects",
+ *     "science" = "science",
+ *     "society" = "society",
+ *     "transport" = "transport",
+ *     "open-data" = "open-data",
+ *     "open-dialog" = "open-dialog",
+ *     "open-govt" = "open-govt",
+ *     "open-info" = "open-info",
+ *   },
+ *   provider = "opengov",
+ *   url = "http://open-data.github.io",
+ *   version = "v1.0",
+ *   path = "",
+ *   renderer = "sprite",
+ *   settings = {
+ *    "tag" = "i",
+ *   },
+ *   attached = {},
+ * )
+ */
+class OpenGovSet extends IconSetBase {
+
+}
