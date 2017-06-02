31bf1fda6567775f86b1b269ba383fe067c1bfd6
Salman Jaffery <mjaffery@gmail.com>
2017-06-02 14:29:28 -0400

    Added custom fields (#53)

---
+++ modules/custom/od_ext/od_ext_analytics/src/Plugin/views/field/FieldOrgName.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\od_ext_analytics\Plugin\views\field;
+
+/**
+ * @file
+ * Definition of Drupal\od_ext_analytics\Plugin\views\field\FieldOrgName.
+ */
+
+use Drupal\google_analytics_reports\Plugin\views\field\GoogleAnalyticsStandard;
+use Drupal\views\ResultRow;
+
+/**
+ * Field handler to flag the node type.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("od_ext_analytics_field_org_name")
+ */
+class FieldOrgName extends GoogleAnalyticsStandard {
+
+  /**
+   * Query to run for this field.
+   */
+  public function query() {
+    // Leave empty to avoid a query on this field.
+  }
+
+  /**
+   * Render the organization name but selecting it from the remote enttiy.
+   */
+  public function render(ResultRow $values) {
+    $uuid = $values->dimension3;
+    $title = '';
+    $uri = 'http://open.canada.ca/data/api/3/action/organization_show?id=' . $uuid;
+    try {
+      $response = \Drupal::httpClient()->get($uri, ['headers' => ['Accept' => 'text/plain']]);
+      $data = (string) $response->getBody();
+      if (empty($data)) {
+        return FALSE;
+      }
+    }
+    catch (RequestException $e) {
+      return FALSE;
+    }
+    $data = json_decode($data, TRUE);
+    if (isset($data['result'])) {
+      $data = $data['result'];
+      $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
+      $title = $data['title'];
+      $title = explode('|', $title);
+      if ($language == 'en') {
+        $title = trim($title[0]);
+      }
+      else {
+        $title = trim($title[1]);
+      }
+    }
+    return $title;
+  }
+
+}
---
+++ modules/custom/od_ext/od_ext_analytics/src/Plugin/views/field/FieldRecordName.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\od_ext_analytics\Plugin\views\field;
+
+/**
+ * @file
+ * Definition of Drupal\od_ext_analytics\Plugin\views\field\FieldRecordName.
+ */
+
+use Drupal\google_analytics_reports\Plugin\views\field\GoogleAnalyticsStandard;
+use Drupal\views\ResultRow;
+
+/**
+ * Field handler to flag the node type.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("od_ext_analytics_field_rec_name")
+ */
+class FieldRecordName extends GoogleAnalyticsStandard {
+
+  /**
+   * Query to run for this field.
+   */
+  public function query() {
+    // Leave empty to avoid a query on this field.
+  }
+
+  /**
+   * Render the organization name but selecting it from the remote enttiy.
+   */
+  public function render(ResultRow $values) {
+    $uuid = $values->dimension2;
+    $record_title = '';
+    $uri = 'http://open.canada.ca/data/api/3/action/package_show?id=' . $uuid;
+
+    try {
+      $response = \Drupal::httpClient()->get($uri, ['headers' => ['Accept' => 'text/plain']]);
+      $data = (string) $response->getBody();
+      if (empty($data)) {
+        return FALSE;
+      }
+    }
+    catch (RequestException $e) {
+      return FALSE;
+    }
+
+    $data = json_decode($data, TRUE);
+    if (isset($data['result'])) {
+      $data = $data['result'];
+      $language = \Drupal::languageManager()->getCurrentLanguage()->getId();
+      $record_title = $data['title_translated'][$language];
+    }
+    return $record_title;
+  }
+
+}
