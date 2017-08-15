6f1b1e21fb48a7e1ce7d0b769f7ffb59c2c2eb22
Salman Jaffery <mjaffery@gmail.com>
2017-08-15 11:16:36 -0400

    Taxonomy layouts (#107)

--- modules/custom/od_ext/od_ext_migration/src/Plugin/migrate/source/TaxonomyTermData.php
+++ modules/custom/od_ext/od_ext_migration/src/Plugin/migrate/source/TaxonomyTermData.php
@@ -144,8 +144,20 @@ class TaxonomyTermData extends SqlBase {
       ->execute()
       ->fetchCol();
 
+    // Links.
+    $links = $this->select('field_data_field_taxonomy_links', 'db')
+      ->fields('db', ['field_taxonomy_links_url', 'field_taxonomy_links_title'])
+      ->condition('entity_id', $row->getSourceProperty('tid'))
+      ->condition('revision_id', $row->getSourceProperty('tid'))
+      ->condition('language', $row->getSourceProperty('language'))
+      ->condition('bundle', $vid)
+      ->execute()
+      ->fetchAllAssoc('links');
+
     $row->setSourceProperty('vid', $vid);
     $row->setSourceProperty('parent_id', $parent_id[0]);
+
+    $row->setSourceProperty('links', $links);
 
     return parent::prepareRow($row);
   }
